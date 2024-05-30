ae95dfd64ed562a4662ca122ccb23c7c3b9c2423
Mikhail Dubovikov <dubovikov94@gmail.com>
2024-05-30 22:39:17 +0300

    Implementation of the SavedFragment

--- app/src/main/java/com/mdubovikov/recipes/presentation/saved/SavedFragment.kt
+++ app/src/main/java/com/mdubovikov/recipes/presentation/saved/SavedFragment.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.mdubovikov.recipes.databinding.FragmentSavedBinding
+import com.mdubovikov.recipes.domain.model.MealModel
+import com.mdubovikov.recipes.presentation.adapters.MealAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class SavedFragment : Fragment() {
 
     private lateinit var binding: FragmentSavedBinding
+    private val viewModel: SavedViewModel by viewModels()
+    private val mealAdapter: MealAdapter by lazy { MealAdapter(::onMealClick, ::onSaveIconClicked) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +29,45 @@ class SavedFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSavedBinding.inflate(inflater, container, false)
-        return binding.root
+        val view = binding.root
+        observeFavoriteMeals()
+        return view
+    }
+
+    private fun observeFavoriteMeals() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.savedMeals.observe(viewLifecycleOwner) { meals ->
+                    meals.let {
+                        if (it.isNotEmpty()) {
+                            binding.tvNoSaved.visibility = View.GONE
+                        } else {
+                            binding.tvNoSaved.visibility = View.VISIBLE
+                        }
+                        mealAdapter.submitList(it)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvSaved.adapter = mealAdapter
+    }
+
+    private fun onMealClick(mealId: Int) {
+        val action =
+            SavedFragmentDirections.actionSavedFragmentToDetailsFragment(mealId = mealId)
+        findNavController().navigate(action)
+    }
+
+    private fun onSaveIconClicked(meal: MealModel) {
+        viewModel.savedIconClicked(meal)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.rvSaved.adapter = null
     }
 }
