54e9b5b3c09420367ff10839f1adc38ad9051faa
Mikhail Dubovikov <dubovikov94@gmail.com>
2024-05-30 22:17:25 +0300

    Creating ViewModel for Saved Screen

---
+++ app/src/main/java/com/mdubovikov/recipes/presentation/saved/SavedViewModel.kt
@@ -0,0 +1,30 @@
+package com.mdubovikov.recipes.presentation.saved
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.mdubovikov.recipes.domain.model.MealModel
+import com.mdubovikov.recipes.domain.use_case.AddOrRemoveMealUseCase
+import com.mdubovikov.recipes.domain.use_case.GetSavedMealsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SavedViewModel @Inject constructor(
+    getSavedMealsUseCase: GetSavedMealsUseCase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main,
+    private val addOrRemoveMealUseCase: AddOrRemoveMealUseCase
+) : ViewModel() {
+
+    val savedMeals: LiveData<List<MealModel>> = getSavedMealsUseCase.invoke().asLiveData()
+
+    fun savedIconClicked(meal: MealModel) {
+        viewModelScope.launch(dispatcher) {
+            addOrRemoveMealUseCase.addOrRemoveMeal(meal)
+        }
+    }
+}
