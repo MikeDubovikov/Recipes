daf8ad74cbaa1e9e4a8a5e7a80372393798eb6b7
Mikhail Dubovikov <dubovikov94@gmail.com>
2024-09-02 22:21:06 +0300

    Move data store to di

--- app/src/main/java/com/mdubovikov/recipes/common/data_store/PreferencesKeys.kt
+++
@@ -1,8 +0,0 @@
-package com.mdubovikov.recipes.common.data_store
-
-import androidx.datastore.preferences.core.intPreferencesKey
-
-object PreferencesKeys {
-    val theme = intPreferencesKey("theme")
-    val language = intPreferencesKey("language")
-}
